46134542d8874395050e748c1a01f9ec08d5efd0
Jeremy Ellis <jellis@hpssapps.com>
2014-10-13 22:18:03 -0700

    Update c-git-pull.sh

--- scripts/c-git-pull.sh
+++ scripts/c-git-pull.sh
@@ -1,4 +1,4 @@
 echo "Must be in the correct directory for your repository"
-echo "Must be have already run git-set-only.sh"
+echo "Must be have already run b-git-setup.sh"
 
 git pull origin master
